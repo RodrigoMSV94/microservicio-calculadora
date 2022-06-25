c8d36e3ad208bb5959c8446252d801820839213f
rodrigo.serrano <rodrigo.serrano.vega.contractor@bbva.com>
2022-06-25 18:03:46 +0200

    Se agrega las depedencias principales para el proyecto

---
+++ src/test/java/com/calculadora/MicroservicioCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
